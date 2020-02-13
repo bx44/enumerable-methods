98737e7b9539eb6204d8604452268431be8f620b
Barak <barakbenhamu@gmail.com>
2020-02-13 15:28:10 -0600

    Fixed linter errors

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,113 +1,116 @@
 #!/usr/bin/ruby
-# rubocop:disable Style/CaseEquality
 
 module Enumerable
-   def my_each
-    return self.to_enum unless block_given?
-    for x in self
-        yield(x)
-    end
-    self
-   end
-   
-   def my_each_with_index
-    return self.to_enum unless block_given?
-    i = 0
-    for x in self
-        yield(x, i)
-        i += 1
-    end
-    self
-   end
+  def my_each
+    return to_enum unless block_given?
 
-   def my_select
-    return self.to_enum unless block_given?
+    for x in self # rubocop:disable Style/For
+      yield(x)
+    end
+    self
+  end
+
+  def my_each_with_index
+    return to_enum unless block_given?
+
+    i = 0
+    for x in self # rubocop:disable Style/For
+      yield(x, i)
+      i += 1
+    end
+    self
+  end
+
+  def my_select
+    return to_enum unless block_given?
+
     new_array = []
-    self.my_each do |x|
-        new_array.push(x) if yield(x)
+    my_each do |x|
+      new_array.push(x) if yield(x)
     end
     new_array
-   end
+  end
 
-   def my_all?
+  def my_all?
     unless block_given?
-        self.my_each do |x|
-            return false unless x
-        end
-        return true
+      my_each do |x|
+        return false unless x
+      end
+      return true
     end
-    self.my_each do |x|
-        return false unless yield(x)
+    my_each do |x|
+      return false unless yield(x)
     end
     true
-   end
+  end
 
-   def my_any?
+  def my_any?
     unless block_given?
-        self.my_each do |x|
-            return true if x
-        end
-        return false
+      my_each do |x|
+        return true if x
+      end
+      return false
     end
-    self.my_each do |x|
-        return true if yield(x)
+    my_each do |x|
+      return true if yield(x)
     end
     false
-   end
+  end
 
-   def my_none?
+  def my_none?
     unless block_given?
-        self.my_each do |x|
-            return false if x
-        end
-        return true
+      my_each do |x|
+        return false if x
+      end
+      return true
     end
-    self.my_each do |x|
-        return false if yield(x)
+    my_each do |x|
+      return false if yield(x)
     end
     true
-   end
+  end
 
-   def my_count(item=nil)
+  def my_count(item = nil)
     count = 0
-    if !block_given? || item != nil
-        return self.length unless item
-        self.my_each do |x|
-            count += 1 if x == item
-        end
-        return count
+    if !block_given? || !item.nil?
+      return length unless item
+
+      my_each do |x|
+        count += 1 if x == item
+      end
+      return count
     end
-    self.my_each do |x|
-        count += 1 if yield(x)
+    my_each do |x|
+      count += 1 if yield(x)
     end
     count
-   end
+  end
 
-   def my_map(proc=nil)
-    return self.to_enum if !block_given? && !proc
+  def my_map(proc = nil)
+    return to_enum if !block_given? && !proc
+
     new_array = []
-    self.my_each do |x|
-        if proc
-            new_array.push proc.call(x)
-        else
-            new_array.push yield(x)
-        end
+    my_each do |x|
+      if proc
+        new_array.push proc.call(x)
+      else
+        new_array.push yield(x)
+      end
     end
     new_array
-   end
+  end
 
-   def my_inject(initial=nil)
+  def my_inject(initial = nil)
     initial ||= self[0]
-    self.my_each do |x|
-        initial = yield(initial, x)
+    my_each do |x|
+      initial = yield(initial, x)
     end
     initial
-   end
-
+  end
 end
 
 def multiply_els(arr)
-    arr.my_inject do |memo, x|
-        memo *= x
-    end
+  arr.my_inject do |memo, x|
+    memo * x
+  end
 end
